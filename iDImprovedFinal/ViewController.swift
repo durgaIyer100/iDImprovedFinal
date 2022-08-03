9d16ebd5da10431046f1ae1950797e1a985aa70d
durgaIyer100 <mdurga1324@gmail.com>
2022-08-03 14:02:05 -0400

    Fix page to and sign in and need to add incorrect password label

--- iDImprovedFinal/ViewController.swift
+++ iDImprovedFinal/ViewController.swift
@@ -14,18 +14,35 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    var username = "janedoe35"
+    var password = "lem0nju*c3"
+    
+    
     @IBAction func signInButton(_ sender: UIButton) {
+
+        if (userName.text == username && passWord.text == password){
+           
+            self.performSegue(withIdentifier: "SignInSegue", sender: self)
+            
+        } else{
+            
+          // make incorrect password label
+            
+        }
+
+        
+        
     }
     
     @IBOutlet weak var userName: UITextField!
     
-    @IBAction func userName(_ sender: UITextField) {
+    @IBAction func userName(_ sender: Any) {
     }
     
     
     @IBOutlet weak var passWord: UITextField!
     
-    @IBAction func passWord(_ sender: UITextField) {
+    @IBAction func passWord(_ sender: Any) {
     }
     
 
--- iDImprovedFinal/ViewControllerTwo.swift
+++ iDImprovedFinal/ViewControllerTwo.swift
@@ -11,17 +11,82 @@ class ViewControllerTwo: UIViewController {
 
     @IBOutlet weak var dietButton: UIButton!
     
+    @IBOutlet weak var health: UIButton!
     
+    @IBOutlet weak var medication: UIButton!
+    
+    @IBOutlet weak var assisstance: UIButton!
+    
+    @IBOutlet weak var emergencyContact: UIButton!
+    
+    @IBOutlet weak var address: UIButton!
+    
+    @IBOutlet weak var infoBox: UILabel!
+    
+    @IBOutlet weak var clear: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        infoBox.isHidden = true
 
         // Do any additional setup after loading the view.
     }
     
     @IBAction func dietButton(_ sender: Any) {
+        
+        infoBox.isHidden = false
+        
+        infoBox.text = "Allergic to peanuts"
+
+        
     }
     
+    @IBAction func health(_ sender: Any) {
+        
+        infoBox.isHidden = false
+        
+        infoBox.text = "No health conditions"
+
+        
+    }
+    
+    @IBAction func medication(_ sender: Any) {
+        
+        infoBox.isHidden = false
+        
+        infoBox.text = "carries an EpiPen"
+        
+    }
+    
+    @IBAction func assisstance(_ sender: Any) {
+        
+        infoBox.isHidden = false
+        
+        infoBox.text = "No assisstance plans"
+        
+    }
+    
+    @IBAction func emergencyContact(_ sender: Any) {
+        
+        infoBox.isHidden = false
+        
+        infoBox.text = "(736) 555 - 7405"
+        
+    }
+    
+    @IBAction func address(_ sender: Any) {
+        
+        infoBox.isHidden = false
+        
+        infoBox.text = "473 Lucky Lane, Townsville Kentucky"
+        
+    }
+    
+    @IBAction func clear(_ sender: Any) {
+        
+        infoBox.isHidden = true
+    }
     
     
     /*    // MARK: - Navigation
@@ -31,6 +96,15 @@ class ViewControllerTwo: UIViewController {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
     }
+     
+     Diet and Allergens - “Allergic to peanuts”
+     Health - “No health conditions”
+     Medication - “Carries an EpiPen”
+     Assistance - “No assistance plans”
+     Emergency Contact - “(736) 555-7405”
+     Address - “473 Lucky Lane, Townsville Kentucky”
+
+     
     */
 
 }
